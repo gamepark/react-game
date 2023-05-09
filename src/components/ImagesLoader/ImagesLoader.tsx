47f3d44a6c8e28e4923a26c09a5228a6465ed215
Romain Fromi <romain.fromi@gmail.com>
2023-05-09 15:12:45 +0200

    Move ImagesLoader from @gamepark/react-components

---
+++ src/components/ImagesLoader/ImagesLoader.tsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import { useCallback, useRef } from 'react'
+import { Picture } from '../Picture'
+import { useWebP } from '../../hooks'
+
+type Props = {
+  images: string[]
+  onImagesLoad?: () => void
+}
+
+export function ImagesLoader({ images, onImagesLoad }: Props) {
+  const webp = useWebP()
+  const loadCount = useRef(0)
+  const onLoad = useCallback(() => {
+    loadCount.current++
+    if (onImagesLoad && loadCount.current === images.length) {
+      onImagesLoad()
+    }
+  }, [onImagesLoad])
+  if (webp === undefined) return null
+  return (
+    <>
+      {images.map((image, index) => <Picture key={index} src={image} alt="" style={{ display: 'none' }} onLoad={onLoad}/>)}
+    </>
+  )
+}
