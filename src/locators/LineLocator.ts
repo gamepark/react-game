7e169b52995809adff79ba0790cd71335dd4c8b5
Romain Fromi <romain.fromi@gmail.com>
2023-05-18 21:07:20 +0200

    Add a DeltaMax function on LineLocator to limit the line size with a large number of items

--- src/locators/LineLocator.ts
+++ src/locators/LineLocator.ts
@@ -6,10 +6,19 @@ export abstract class LineLocator<P extends number = number, M extends number = 
     const coordinates = this.getCoordinates(item, context)
     const index = this.getItemIndex(item, context)
     const delta = this.getDelta(item, context)
+    if (this.getDeltaMax) {
+      const deltaMax = this.getDeltaMax(item, context)
+      const count = this.countItems(item.location, context)
+      if (deltaMax.x !== undefined) delta.x = Math.min(delta.x ?? 0, deltaMax.x / count)
+      if (deltaMax.y !== undefined) delta.y = Math.min(delta.y ?? 0, deltaMax.y / count)
+      if (deltaMax.z !== undefined) delta.z = Math.min(delta.z ?? 0, deltaMax.z / count)
+    }
     return { x: coordinates.x + index * (delta.x ?? 0), y: coordinates.y + index * (delta.y ?? 0), z: coordinates.z + index * (delta.z ?? 0) }
   }
 
   abstract getCoordinates(item: MaterialItem<P, L>, context: PlaceItemContext<P, M, L>): Coordinates
 
   abstract getDelta(item: MaterialItem<P, L>, context: PlaceItemContext<P, M, L>): Partial<Coordinates>
+
+  getDeltaMax?(item: MaterialItem<P, L>, context: PlaceItemContext<P, M, L>): Partial<Coordinates>
 }
