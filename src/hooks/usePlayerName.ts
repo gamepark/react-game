0388196f1c23da66d821ae38da435e535728dfab
Romain Fromi <romain.fromi@gmail.com>
2023-07-10 14:30:48 +0200

    Make usePlayerName smart enough to fallback on the default option name

--- src/hooks/usePlayerName.ts
+++ src/hooks/usePlayerName.ts
@@ -1,6 +1,15 @@
 import { GamePageState } from '@gamepark/react-client'
 import { useSelector } from 'react-redux'
+import { useContext } from 'react'
+import { gameContext } from '../components'
+import { isWithPlayerIdOptions } from '../../../workshop/packages/rules-api'
+import { useTranslation } from 'react-i18next'
 
 export function usePlayerName<PlayerId = any>(playerId: PlayerId): string {
-  return useSelector((state: GamePageState) => state.players.find(player => player.id === playerId ?? state.playerId)?.name ?? '')
+  const name = useSelector((state: GamePageState) => state.players.find(player => player.id === playerId ?? state.playerId)?.name ?? '')
+  const optionsSpec = useContext(gameContext).optionsSpec
+  const { t } = useTranslation()
+  if (name) return name
+  if (isWithPlayerIdOptions(optionsSpec)) return optionsSpec.players.id.valueSpec(playerId).label(t)
+  return t('Player {number}', { number: playerId })
 }
