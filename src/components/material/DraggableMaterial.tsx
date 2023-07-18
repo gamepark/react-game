af4ce7372994561478c80efca46ca616017ce8ba
Romain Fromi <romain.fromi@gmail.com>
2023-07-18 20:07:14 +0200

    DraggableMaterial now follows parent item being dragged

--- src/components/material/DraggableMaterial.tsx
+++ src/components/material/DraggableMaterial.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { forwardRef, useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { DisplayedItem, isCreateItem, isDeleteItem, isMoveItem, ItemMove, itemsCanMerge, MaterialItem, MaterialMove } from '@gamepark/rules-api'
+import { DisplayedItem, isCreateItem, isDeleteItem, isMoveItem, ItemMove, itemsCanMerge, MaterialItem, MaterialMove, XYCoordinates } from '@gamepark/rules-api'
 import { MaterialComponent, MaterialComponentProps } from './MaterialComponent'
 import { grabbingCursor, grabCursor, pointerCursorCss, transformCss } from '../../css'
-import { DragStartEvent, useDndMonitor, useDraggable } from '@dnd-kit/core'
+import { DragMoveEvent, DragStartEvent, useDndMonitor, useDraggable } from '@dnd-kit/core'
 import { css } from '@emotion/react'
 import { combineEventListeners } from '../../utilities'
 import { useAnimation, useAnimations, useLegalMoves, useMaterialAnimations, useMaterialContext } from '../../hooks'
@@ -11,7 +11,7 @@ import merge from 'lodash/merge'
 import equal from 'fast-deep-equal'
 import { mergeRefs } from 'react-merge-refs'
 import { useTransformContext } from 'react-zoom-pan-pinch'
-import { ItemContext } from '../../locators'
+import { ItemContext, MaterialContext } from '../../locators'
 
 export type DraggableMaterialProps<P extends number = number, M extends number = number, L extends number = number> = {
   item: MaterialItem<P, L>
@@ -27,25 +27,41 @@ export const DraggableMaterial = forwardRef<HTMLDivElement, DraggableMaterialPro
 ) => {
 
   const displayedItem: DisplayedItem = useMemo(() => ({ type, index, displayIndex }), [type, index, displayIndex])
-  const { attributes, listeners, transform, setNodeRef } = useDraggable({
+  const { attributes, listeners, transform: selfTransform, setNodeRef } = useDraggable({
     id: `${type}_${index}_${displayIndex}`,
     data: displayedItem,
     disabled
   })
+
+  const [draggedItem, setDraggedItem] = useState<DisplayedItem>()
+  const context = useMaterialContext()
+  const isDraggingParent = useMemo(() => !!draggedItem && isPlacedOnItem(item, draggedItem, context), [item, draggedItem, context])
+  const legalMoves = useLegalMoves<MaterialMove>()
+  const canDropToSameLocation = useMemo(() => {
+    if (!draggedItem) return false
+    const location = context.locators[item.location.type].locationDescription
+    if (!location) return false
+    const description = context.material[draggedItem.type]
+    const itemContext = { ...context, ...draggedItem }
+    return legalMoves.some(move => description.canDrag(move, itemContext) && location.canDrop(move, item.location, context))
+  }, [item, draggedItem, context, legalMoves])
+
+  const [parentTransform, setParentTransform] = useState<XYCoordinates>()
+  const transform = selfTransform ?? parentTransform
 
   // We need to delay a little the default transition removal when dragging starts, otherwise dnd-kit suffers from transform side effect
   // because we opted out from ignoring transform in the configuration (using: "draggable: { measure: getClientRect }")
   const [ignoreTransform, setIgnoreTransform] = useState(true)
   const [smoothReturn, setSmoothReturn] = useState(false)
   useEffect(() => {
-    if (transform !== null) {
+    if (transform) {
       setSmoothReturn(true)
       const timeout = setTimeout(() => setIgnoreTransform(false))
       return () => clearTimeout(timeout)
     } else {
       setIgnoreTransform(true)
     }
-  }, [transform !== null])
+  }, [!transform])
 
   const transformContext = useTransformContext()
   const transformRef = useRef<string>()
@@ -56,7 +72,6 @@ export const DraggableMaterial = forwardRef<HTMLDivElement, DraggableMaterialPro
   }
 
   const materialAnimations = useMaterialAnimations(type)
-  const context = useMaterialContext()
   const animations = useAnimations<MaterialMove>(animation => animation.action.playerId === context.player)
   const animation = useAnimation<ItemMove>(animation =>
     (isCreateItem(animation.move, type) && itemsCanMerge(item, animation.move.item))
@@ -82,30 +97,13 @@ export const DraggableMaterial = forwardRef<HTMLDivElement, DraggableMaterialPro
     }
   }, [isItemToAnimate])
 
-  const legalMoves = useLegalMoves<MaterialMove>()
-  const [isDragging, setIsDragging] = useState(false)
-  const [canDropToSameLocation, setCanDropToSameLocation] = useState(false)
-
-  const onDragStart = useCallback((event: DragStartEvent) => {
-    setIsDragging(true)
-    const dragged = event.active.data.current
-    if (dataIsDisplayedItem(dragged)) {
-      const locationDescription = locator.locationDescription
-      const draggedItemDescription = context.material[dragged.type]
-      if (locationDescription && legalMoves.some(move =>
-        draggedItemDescription.canDrag(move, { ...context, ...dragged }) && locationDescription.canDrop(move, item.location, context)
-      )) {
-        setCanDropToSameLocation(true)
-      }
-    }
-  }, [context, locator, legalMoves])
-
+  const onDragStart = useCallback((event: DragStartEvent) => dataIsDisplayedItem(event.active.data.current) && setDraggedItem(event.active.data.current), [])
+  const onDragMove = useCallback((event: DragMoveEvent) => isDraggingParent && setParentTransform(event.delta), [isDraggingParent])
   const onDragEnd = useCallback(() => {
-    setIsDragging(false)
-    setCanDropToSameLocation(false)
+    setDraggedItem(undefined)
+    setParentTransform(undefined)
   }, [])
-
-  useDndMonitor({ onDragStart, onDragEnd })
+  useDndMonitor({ onDragStart, onDragEnd, onDragMove })
 
   return (
     <div css={[animationWrapperCss, animationCss]}>
@@ -117,7 +115,7 @@ export const DraggableMaterial = forwardRef<HTMLDivElement, DraggableMaterialPro
                            transformCss(preTransform, applyTransform && transformRef.current, postTransform),
                            canDropToSameLocation && noPointerEvents
                          ]}
-                         highlight={highlight ?? (!disabled && !animations.length && !isDragging)}
+                         highlight={highlight ?? (!disabled && !animations.length && !draggedItem)}
                          {...props} {...attributes} {...combineEventListeners(listeners ?? {}, props)}/>
     </div>
   )
@@ -145,4 +143,14 @@ const transformTransition = css`
 
 export function dataIsDisplayedItem<M extends number = number>(data?: Record<string, any>): data is DisplayedItem<M> {
   return typeof data?.type === 'number' && typeof data?.index === 'number' && typeof data?.displayIndex === 'number'
+}
+
+const isPlacedOnItem = <P extends number = number, M extends number = number, L extends number = number>(
+  childItem: MaterialItem<P, L>, item: DisplayedItem<M>, context: MaterialContext<P, M, L>
+): boolean => {
+  if (childItem.location.parent === undefined) return false
+  const locator = context.locators[childItem.location.type]
+  if (locator.parentItemType === item.type && childItem.location.parent === item.index) return true
+  const parentItem = context.game.items[item.type]![item.index]
+  return isPlacedOnItem(parentItem, item, context)
 }
