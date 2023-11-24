ae5df1e40241bea77040e7af30d4d0f7a8e355c8
Romain Fromi <romain.fromi@gmail.com>
2023-11-24 16:13:51 +0100

    Add a graphicDesigner field in Loading Screen

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,16 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css, Theme } from '@emotion/react'
-import { faLaptopCode, faLightbulb, faPaintBrush, faWrench } from '@fortawesome/free-solid-svg-icons'
+import { faImage } from '@fortawesome/free-solid-svg-icons/faImage'
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons/faLaptopCode'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons/faLightbulb'
+import { faPaintbrush } from '@fortawesome/free-solid-svg-icons/faPaintbrush'
+import { faWrench } from '@fortawesome/free-solid-svg-icons/faWrench'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { HTMLAttributes, useEffect, useState } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
-import { Picture } from '../Picture'
 import { BackgroundTheme } from '../../css'
+import { Picture } from '../Picture'
 
 export type LoadingScreenProps = {
   gameBox?: string
   author?: string | string[]
   artist?: string | string[]
+  graphicDesigner?: string | string[]
   publisher?: string | string[]
   developer?: string | string[]
   display: boolean
@@ -20,6 +25,7 @@ export const LoadingScreen = ({
                                 gameBox = process.env.PUBLIC_URL + '/box-640.png',
                                 author,
                                 artist,
+                                graphicDesigner,
                                 publisher,
                                 developer,
                                 display,
@@ -38,6 +44,7 @@ export const LoadingScreen = ({
   if (!includeInLayout) return null
   const authors = typeof author === 'string' ? [author] : author ?? []
   const artists = typeof artist === 'string' ? [artist] : artist ?? []
+  const graphicDesigners = typeof graphicDesigner === 'string' ? [graphicDesigner] : graphicDesigner ?? []
   const publishers = typeof publisher === 'string' ? [publisher] : publisher ?? []
   const developers = typeof developer === 'string' ? [developer] : developer ?? []
   return (
@@ -53,7 +60,7 @@ export const LoadingScreen = ({
           <Trans defaults="authors.2" values={{ author1: authors[0], author2: authors[1] }} components={[<strong/>]}/>
         }
         <br/>
-        <FontAwesomeIcon css={iconStyle} icon={faPaintBrush}/>
+        <FontAwesomeIcon css={iconStyle} icon={faPaintbrush}/>
         {artists.length === 1 &&
           <Trans defaults="artists.1" values={{ artist }} components={[<strong/>]}/>
         }
@@ -61,6 +68,13 @@ export const LoadingScreen = ({
           <Trans defaults="artists.2" values={{ artist1: artists[0], artist2: artists[1] }} components={[<strong/>]}/>
         }
         <br/>
+        {graphicDesigners.length > 0 && <>
+          <FontAwesomeIcon css={iconStyle} icon={faImage}/>
+          {graphicDesigners.length === 1 &&
+            <Trans defaults="graphics.1" values={{ name: graphicDesigners[0] }} components={[<strong/>]}/>
+          }
+          <br/>
+        </>}
         <FontAwesomeIcon css={iconStyle} icon={faWrench}/>
         {publishers.length === 1 &&
           <Trans defaults="publishers.1" values={{ publisher }} components={[<strong/>]}/>
@@ -106,7 +120,7 @@ const loadingScreenStyle = (theme: Theme) => css`
 const gameBoxStyle = css`
   position: relative;
   width: 62em;
-  height: 66em;
+  height: 62em;
   margin-top: 7em;
   margin-bottom: 2em;
   filter: drop-shadow(1px 0 white) drop-shadow(0 1px white) drop-shadow(-1px 0 white) drop-shadow(0 -1px white);
