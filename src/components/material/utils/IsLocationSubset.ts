b4ccf83bb4cf14a5375cb7c20b6eec4dd51e9479
Patrick Beaume <p.beaume@gmail.com>
2023-11-28 20:11:18 +0100

    remove usused function & make location partial

--- src/components/material/utils/IsLocationSubset.ts
+++ src/components/material/utils/IsLocationSubset.ts
@@ -1,6 +1,6 @@
 import { Location } from '@gamepark/rules-api'
 import equal from 'fast-deep-equal'
 
-export const isLocationSubset = (testLocation: Location, parentLocation: Location) => {
+export const isLocationSubset = (testLocation: Partial<Location>, parentLocation: Location) => {
   return Object.keys(parentLocation).every(key => parentLocation[key] === undefined || equal(parentLocation[key], testLocation[key]))
 }
