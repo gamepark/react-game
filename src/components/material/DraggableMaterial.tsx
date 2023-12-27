609dc0318bcd314d0d1dbdb016d4ed89806a14ff
Patrick Beaumé <p.beaume@gmail.com>
2023-12-27 12:06:49 +0100

    only remove onLongClick = onShortClick in MaterialComponent

--- src/components/material/DraggableMaterial.tsx
+++ src/components/material/DraggableMaterial.tsx
@@ -57,8 +57,7 @@ export const DraggableMaterial = forwardRef<HTMLDivElement, DraggableMaterialPro
     const longClickMoves = isAnimatingPlayerAction ? [] : legalMoves.filter(move => material[type]?.canLongClick(move, itemContext))
     const openRules = () => play(displayMaterialHelp(type, item, index, displayIndex), { local: true })
     const onShortClick = undoSelectItem ?? (shortClickMoves.length === 1 ? () => play(shortClickMoves[0]) : openRules)
-    const longClickMustOpenRules = (undoSelectItem || shortClickMoves.length === 1) && onShortClick !== openRules
-    const onLongClick = longClickMustOpenRules ? openRules : longClickMoves.length === 1 ? () => play(longClickMoves[0]) : () => undefined
+    const onLongClick = (undoSelectItem || shortClickMoves.length === 1) ? openRules : longClickMoves.length === 1 ? () => play(longClickMoves[0]) : undefined
     return [onShortClick, onLongClick, shortClickMoves.length === 1 || longClickMoves.length === 1]
   }, [legalMoves, itemContext, isAnimatingPlayerAction, play])
 
