0c2ff7a861ba91a7687e48f76abdfb00af2146d0
Patrick Beaume <p.beaume@gmail.com>
2024-01-17 12:12:31 +0100

    improve performance

--- src/components/material/MaterialComponent.tsx
+++ src/components/material/MaterialComponent.tsx
@@ -1,10 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react'
-import { MaterialGame } from '@gamepark/rules-api'
-import { forwardRef, HTMLAttributes, MouseEvent } from 'react'
+import { css, Interpolation, Theme } from '@emotion/react'
+import { forwardRef, HTMLAttributes, MouseEvent, useMemo } from 'react'
 import { LongPressCallbackReason, LongPressEventType, useLongPress } from 'use-long-press'
 import { sizeCss } from '../../css'
-import { useGame, useMaterialContext, useMaterialDescription } from '../../hooks'
+import { useMaterialContext, useMaterialDescription } from '../../hooks'
 import { combineEventListeners } from '../../utilities'
 
 export type MaterialComponentProps<M extends number = number, ItemId = any> = {
@@ -14,12 +13,12 @@ export type MaterialComponentProps<M extends number = number, ItemId = any> = {
   onLongClick?: () => void
   highlight?: boolean
   playDown?: boolean
+  css?: Interpolation<Theme>
 } & HTMLAttributes<HTMLElement>
 
 export const MaterialComponent = forwardRef<HTMLDivElement, MaterialComponentProps>((
-  { type, itemId, onShortClick, onLongClick, highlight, playDown, ...props }, ref
+  { type, itemId, onShortClick, onLongClick, highlight, playDown, css , ...props }, ref
 ) => {
-  const game = useGame<MaterialGame>()
   const description = useMaterialDescription(type)
   const context = useMaterialContext()
 
@@ -35,12 +34,14 @@ export const MaterialComponent = forwardRef<HTMLDivElement, MaterialComponentPro
     filterEvents: event => !(event as MouseEvent).button // Ignore clicks on mouse buttons > 0
   })()
 
-  if (!description || !game) return null
+  if (!description) return null
 
   const { width, height } = description.getSize(itemId, context)
 
+  const componentCss = useMemo(() => [materialCss, sizeCss(width, height), css], [width, height, css])
+
   return (
-    <div ref={ref} css={[materialCss, sizeCss(width, height)]} {...props} {...combineEventListeners(listeners, props)}>
+    <div ref={ref} css={componentCss} {...props} {...combineEventListeners(listeners, props)}>
       {description.content({ itemId, context, highlight, playDown, ...props })}
     </div>
   )
