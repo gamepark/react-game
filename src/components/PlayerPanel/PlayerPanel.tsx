539df867e2d1ad96a63c8b4132d44fc2cf5e7024
Patrick Beaumé <p.beaume@gmail.com>
2024-01-17 13:50:53 +0100

    Add active ring around avatar to indicate that the player is active (#23)

--- src/components/PlayerPanel/PlayerPanel.tsx
+++ src/components/PlayerPanel/PlayerPanel.tsx
@@ -1,22 +1,29 @@
 /** @jsxImportSource @emotion/react */
-import { HTMLAttributes } from 'react'
+import { css, keyframes } from '@emotion/react'
+import { MaterialRules } from '@gamepark/rules-api'
+import { FC, HTMLAttributes } from 'react'
 import { usePlayerName, useRules } from '../../hooks'
-import { GamePoints } from '../GamePoints'
 import { Avatar, SpeechBubbleDirection } from '../Avatar'
+import { GamePoints } from '../GamePoints'
 import { PlayerTimer } from '../PlayerTimer'
-import { css } from '@emotion/react'
 
-export type PlayerPanelProps<PlayerId = any> = {
+export type PlayerPanelProps<PlayerId extends number = number> = {
   playerId: PlayerId
-  color?: string
+  color?: string,
+  activeRing?: boolean
 } & HTMLAttributes<HTMLDivElement>
 
-export const PlayerPanel = <PlayerId extends any>({ playerId, color = '#28B8CE', children, ...props }: PlayerPanelProps<PlayerId>) => {
+export const PlayerPanel: FC<PlayerPanelProps> = (p) => {
+  const { playerId, activeRing, color = '#28B8CE', children, ...props } = p
   const playerName = usePlayerName(playerId)
-  const rules = useRules()
+  const rules = useRules<MaterialRules>()
+  const isTurnToPlay = rules?.isTurnToPlay(playerId) ?? false
   return (
-    <div css={panelPlayerStyle(color, rules?.isTurnToPlay(playerId))} {...props}>
+    <div css={panelPlayerStyle(color, isTurnToPlay)} {...props}>
       <Avatar css={avatarStyle} playerId={playerId} speechBubbleProps={{ direction: SpeechBubbleDirection.BOTTOM_LEFT }}/>
+      {activeRing && isTurnToPlay && <div css={isPlaying}>
+        <div css={isTurnToPlay && circle}/>
+      </div>}
       <h2 css={nameStyle}>{playerName}</h2>
       {!rules?.isOver() && <PlayerTimer playerId={playerId} css={timerStyle}/>}
       <GamePoints playerId={playerId} css={gamePointCss}/>
@@ -41,7 +48,43 @@ const avatarStyle = css`
   height: 6em;
   width: 6em;
   color: black;
+  z-index: 1;
 `
+
+const isPlaying = css`
+  position: absolute;
+  top: -0.1em;
+  left: 0;
+  border-radius: 50%;
+  height: 6em;
+  width: 6em;
+  color: black;
+  z-index: 0;
+`
+
+const circleAnimation = keyframes`
+  to {
+    transform: rotateZ(0);
+  }
+  from {
+    transform: rotateZ(360deg);
+  }
+`
+
+const inset = 0.8
+const circle = css`
+  background-image: linear-gradient(
+          to bottom, gold 0%,
+          rgb(40, 184, 206) 100%);
+  position: absolute;
+  top: -${inset}em;
+  bottom: -${inset}em;
+  right: -${inset}em;
+  left: -${inset}em;
+  border-radius: inherit;
+  animation: ${circleAnimation} 1s infinite linear;
+`
+
 
 const nameStyle = css`
   position: absolute;
