6aacbcab9ca87bd7ef1f121766cac382e9594327
Patrick Beaume <p.beaume@gmail.com>
2024-04-17 13:20:01 +0200

    prevent double click by using pending instead of delayed

--- src/components/material/utils/useIsAnimatingPlayerAction.tsx
+++ src/components/material/utils/useIsAnimatingPlayerAction.tsx
@@ -1,11 +1,11 @@
+import { GamePageState } from '@gamepark/react-client'
 import { useSelector } from 'react-redux'
-import { GamePageState } from '../../../../../workshop/packages/react-client'
 
 export const useIsAnimatingPlayerAction = (): boolean => {
   return useSelector((state: GamePageState) => {
     if (state.playerId === undefined || !state.actions) return false
     return state.actions.some(action =>
-      action.playerId === state.playerId && (action.delayed || action.animation !== undefined)
+      action.playerId === state.playerId && (action.pending || action.animation !== undefined)
     )
   })
 }
