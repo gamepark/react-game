39d32a6c5d9ed29f1e7bb176ae1b9081271424fe
Patrick Beaume <p.beaume@gmail.com>
2024-04-18 18:38:35 +0200

    create non existant static location if needed

--- src/components/material/GameTable/focus/LocationsWithFocus.ts
+++ src/components/material/GameTable/focus/LocationsWithFocus.ts
@@ -1,6 +1,6 @@
 import { Location, MaterialItem } from '@gamepark/rules-api'
 import equal from 'fast-deep-equal'
-import { ItemContext } from '../../../../locators'
+import { ItemContext, LocationContext } from '../../../../locators'
 import { MaterialFocus } from './MaterialFocus'
 
 export type LocationsWithFocus = {
@@ -8,13 +8,7 @@ export type LocationsWithFocus = {
   focusedIndexes: number[]
 }
 
-export const getLocationsWithFocus = (
-  item: MaterialItem, context: ItemContext, focus?: MaterialFocus
-): LocationsWithFocus => {
-  const locationsFocus = focus?.locations?.filter(location =>
-    context.locators[location.type]?.parentItemType === context.type && (location.parent ?? 0) === context.index
-  ) ?? []
-  const locations = context.material[context.type]?.getLocations(item, context) ?? []
+const getFocusedLocations = (locationsFocus: Location[], locations: Location[]) => {
   const focusedIndexes = []
   for (const locationFocus of locationsFocus) {
     const index = locations.findIndex(location => equal(location, locationFocus))
@@ -27,3 +21,25 @@ export const getLocationsWithFocus = (
   }
   return { locations, focusedIndexes }
 }
+
+export const getLocationsWithFocus = (
+  item: MaterialItem, context: ItemContext, focus?: MaterialFocus
+): LocationsWithFocus => {
+  const locationsFocus = focus?.locations?.filter(location =>
+    context.locators[location.type]?.parentItemType === context.type && (location.parent ?? 0) === context.index
+  ) ?? []
+  const locations = context.material[context.type]?.getLocations(item, context) ?? []
+  return getFocusedLocations(locationsFocus, locations)
+}
+
+export const getStaticLocationsWithFocus = (
+  context: LocationContext, focus?: MaterialFocus
+): Location[] => {
+  return Object.entries(context.locators).flatMap(([type]) => {
+    const locationsFocus = focus?.locations.filter(l => l.type === +type) ?? []
+    const locations = context.locators[type]?.getLocationDescription(context)?.getLocations(context) ?? []
+    return getFocusedLocations(locationsFocus, locations).locations
+  })
+
+}
+
