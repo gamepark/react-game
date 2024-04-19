cca0c8cb018d0b8edfc0689bba3d257524d5d7e8
Patrick Beaumé <p.beaume@gmail.com>
2024-04-19 10:15:59 +0200

    create non existant static location if needed (#72)
    * create non existant static location if needed
    
    * add locator intermediate var

--- src/components/material/GameTable/StaticLocationsDisplay.tsx
+++ src/components/material/GameTable/StaticLocationsDisplay.tsx
@@ -3,19 +3,27 @@ import { Interpolation, Theme } from '@emotion/react'
 import equal from 'fast-deep-equal'
 import { useMaterialContext } from '../../../hooks'
 import { SimpleDropArea } from '../locations'
-import { useFocusContext } from './focus'
+import { getStaticLocationsWithFocus, useFocusContext } from './focus'
 
 export const StaticLocationsDisplay = (props: { css?: Interpolation<Theme> }) => {
   const context = useMaterialContext()
   const { focus, addFocusRef } = useFocusContext()
-  return <>
-    {Object.values(context.locators).map(locator => {
-        return locator?.getLocationDescription(context)?.getLocations(context).map(location => {
+  const locations = getStaticLocationsWithFocus(context, focus)
+  return (
+    <>
+      {
+        locations.map(location => {
           const isFocused = focus?.locations.some(focusedLocation => equal(focusedLocation, location))
-          return <SimpleDropArea key={JSON.stringify(location)} location={location} alwaysVisible={isFocused} ref={isFocused ? addFocusRef : undefined}
-                                 {...props}/>
+          return (
+            <SimpleDropArea
+              key={JSON.stringify(location)}
+              location={location}
+              alwaysVisible={isFocused}
+              ref={isFocused ? addFocusRef : undefined}
+              {...props}/>
+          )
         })
       }
-    )}
-  </>
+    </>
+  )
 }
--- src/components/material/GameTable/focus/LocationsWithFocus.ts
+++ src/components/material/GameTable/focus/LocationsWithFocus.ts
@@ -1,6 +1,6 @@
 import { Location, MaterialItem } from '@gamepark/rules-api'
 import equal from 'fast-deep-equal'
-import { ItemContext } from '../../../../locators'
+import { ItemContext, LocationContext } from '../../../../locators'
 import { MaterialFocus } from './MaterialFocus'
 
 export type LocationsWithFocus = {
@@ -8,13 +8,7 @@ export type LocationsWithFocus = {
   focusedIndexes: number[]
 }
 
-export const getLocationsWithFocus = (
-  item: MaterialItem, context: ItemContext, focus?: MaterialFocus
-): LocationsWithFocus => {
-  const locationsFocus = focus?.locations?.filter(location =>
-    context.locators[location.type]?.parentItemType === context.type && (location.parent ?? 0) === context.index
-  ) ?? []
-  const locations = context.material[context.type]?.getLocations(item, context) ?? []
+const getFocusedLocations = (locationsFocus: Location[], locations: Location[]) => {
   const focusedIndexes = []
   for (const locationFocus of locationsFocus) {
     const index = locations.findIndex(location => equal(location, locationFocus))
@@ -27,3 +21,26 @@ export const getLocationsWithFocus = (
   }
   return { locations, focusedIndexes }
 }
+
+export const getLocationsWithFocus = (
+  item: MaterialItem, context: ItemContext, focus?: MaterialFocus
+): LocationsWithFocus => {
+  const locationsFocus = focus?.locations?.filter(location =>
+    context.locators[location.type]?.parentItemType === context.type && (location.parent ?? 0) === context.index
+  ) ?? []
+  const locations = context.material[context.type]?.getLocations(item, context) ?? []
+  return getFocusedLocations(locationsFocus, locations)
+}
+
+export const getStaticLocationsWithFocus = (
+  context: LocationContext, focus?: MaterialFocus
+): Location[] => {
+  return Object.keys(context.locators).flatMap((type) => {
+    const locator = context.locators[type]
+    const locationsFocus = focus?.locations.filter(l => l.type === +type) ?? []
+    const locations = locator.getLocationDescription(context)?.getLocations(context) ?? []
+    return getFocusedLocations(locationsFocus, locations).locations
+  })
+
+}
+
