8219020d83bad6e3cdf49bd28bb0c497d89bb0a5
Romain Fromi <romain.fromi@gmail.com>
2024-06-27 17:29:55 +0200

    Fix adjusting rotation when item is place on a rotated parent

--- src/components/material/animations/adjustRotation.ts
+++ src/components/material/animations/adjustRotation.ts
@@ -1,33 +1,20 @@
-import { Coordinates } from '@gamepark/rules-api'
-
 export const adjustRotation = (targetTransforms: string[], sourceTransforms: string[]): string[] => {
-  const result: string[] = []
-  const sourceRotation = sumRotationsDegrees(sourceTransforms)
-  const targetRotation = sumRotationsDegrees(targetTransforms)
-  for (const axis in sourceRotation) {
-    const delta = Math.round((sourceRotation[axis] - targetRotation[axis]) / 360)
-    if (delta) result.push(`rotate${axis.toUpperCase()}(${delta * 360}deg)`)
-  }
-  return targetTransforms.concat(result)
+  return targetTransforms.map((value, i) => toClosestRotation(value, sourceTransforms[i]))
 }
 
-const sumRotationsDegrees = (transforms: string[]): Coordinates => {
-  const rotations: Coordinates = { x: 0, y: 0, z: 0 }
-  for (const transform of transforms) {
-    const rotateMatch = transform.match(/rotate([^(]*)\((-?\d+\.?\d*)([^)]*)\)/)
-    if (rotateMatch) {
-      const axis = rotateMatch[1].toLowerCase(), value = parseFloat(rotateMatch[2]), unit = rotateMatch[3]
-      if (axis in rotations) {
-        switch (unit) {
-          case 'deg':
-            rotations[axis] += value
-            break
-          case 'rad':
-            rotations[axis] += value * 180 / Math.PI
-            break
-        }
-      }
-    }
-  }
-  return rotations
+const toClosestRotation = (target: string, source: string) => {
+  if (!source) return target
+  const sourceMatch = source.match(/rotate([^(]*)\((-?\d+\.?\d*)([^)]*)\)/)
+  const targetMatch = target.match(/rotate([^(]*)\((-?\d+\.?\d*)([^)]*)\)/)
+  if (!(sourceMatch && targetMatch)) return target
+  const sourceAxis = sourceMatch[1].toLowerCase(), targetAxis = targetMatch[1].toLowerCase()
+  if (!targetAxis || sourceAxis !== targetAxis) return target
+  const sourceRotation = getRotationDegrees(parseFloat(sourceMatch[2]), sourceMatch[3])
+  const targetRotation = getRotationDegrees(parseFloat(targetMatch[2]), targetMatch[3])
+  const delta = Math.round((sourceRotation - targetRotation) / 360)
+  if (delta === 0) return target
+  return `rotate${targetAxis}(${targetRotation + delta * 360}deg)`
 }
+
+const getRotationDegrees = (value: number, unit: string) => unit === 'rad' ? value * 180 / Math.PI : value
+
