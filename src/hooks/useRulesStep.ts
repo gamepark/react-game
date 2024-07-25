955361753d514d82fc6e2934ac72f6a896075b73
Romain Fromi <romain.fromi@gmail.com>
2024-07-25 14:22:54 +0200

    Remove unused code: useRulesStep

--- src/hooks/useRulesStep.ts
+++
@@ -1,7 +0,0 @@
-import { MaterialRules, MaterialRulesPart } from '@gamepark/rules-api'
-import { useRules } from './useRules'
-
-export function useRulesStep<T extends MaterialRulesPart>(): T | undefined {
-  const rules = useRules<MaterialRules>()
-  return rules?.rulesStep as T | undefined
-}
