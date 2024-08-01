7e40f35954cdd0c67985a64782981d5d938f11cc
Romain Fromi <romain.fromi@gmail.com>
2024-08-01 15:51:57 +0200

    Move legalMoves caching from useLegalMoves hook to the game reducer

--- src/hooks/useLegalMoves.ts
+++ src/hooks/useLegalMoves.ts
@@ -1,34 +1,14 @@
 import { GamePageState } from '@gamepark/react-client'
-import { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { useIsAnimatingPlayerAction } from '../components/material/utils/useIsAnimatingPlayerAction'
-import { usePlayerId } from './usePlayerId'
-import { useRules } from './useRules'
 
-type Cache<Game = any, Move = any> = {
-  game: Game
-  moves: Move[]
-}
-
-const cache: Cache = {
-  game: undefined,
-  moves: []
-}
 
 export function useLegalMoves<Move = any>(predicate?: (move: Move) => boolean): Move[] {
-  const rules = useRules()
-  const playerId = usePlayerId()
+  const legalMoves = useSelector((state: GamePageState<any, Move>) => state.legalMoves)
   const gameOver = useSelector((state: GamePageState) => state.gameOver)
   const isAnimatingPlayerAction = useIsAnimatingPlayerAction()
-  if (rules && playerId !== undefined && cache.game !== rules.game && !isAnimatingPlayerAction) {
-    cache.game = rules.game
-    cache.moves = rules.getLegalMoves(playerId)
-  }
-  return useMemo(() => {
-      if (gameOver || isAnimatingPlayerAction) return []
-      return predicate ? cache.moves.filter(predicate) : cache.moves
-    },
-    [cache.moves, predicate, gameOver, isAnimatingPlayerAction])
+  if (gameOver || isAnimatingPlayerAction) return []
+  return predicate ? legalMoves.filter(predicate) : legalMoves
 }
 
 export function useLegalMove<Move = any>(predicate?: (move: Move) => boolean): Move | undefined {
