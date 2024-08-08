a46fca6ceb049479d17b9ac2c3afd2396a7b56f0
Romain Fromi <romain.fromi@gmail.com>
2024-08-08 17:00:52 +0200

    Make HandLocator able to place locations inside the hand

--- src/locators/HandLocator.ts
+++ src/locators/HandLocator.ts
@@ -1,14 +1,18 @@
-import { Coordinates, MaterialItem } from '@gamepark/rules-api'
-import { ItemContext, Locator } from './Locator'
+import { Coordinates, Location, MaterialItem } from '@gamepark/rules-api'
+import { ItemContext, Locator, MaterialContext } from './Locator'
 
 export abstract class HandLocator<P extends number = number, M extends number = number, L extends number = number> extends Locator<P, M, L> {
   getItemCoordinates(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): Coordinates {
-    const { x = 0, y = 0, z = 0 } = this.getCoordinates(item.location, context)
-    const index = this.getItemIndex(item, context)
-    const deltaZ = this.getDeltaZ(item, context)
-    const radius = this.getRadius(item, context)
-    const baseAngle = this.getBaseAngle(item, context)
-    const angle = this.getItemAngle(item, context)
+    return this.getLocationCoordinates(item.location, context)
+  }
+
+  getLocationCoordinates(location: Location<P, L>, context: MaterialContext<P, M, L>): Coordinates {
+    const { x = 0, y = 0, z = 0 } = this.getCoordinates(location, context)
+    const index = this.getLocationIndex(location, context) ?? 0
+    const deltaZ = this.getDeltaZ(location, context)
+    const radius = this.getRadius(location, context)
+    const baseAngle = this.getBaseAngle(location, context)
+    const angle = this.getRotateZ(location, context)
     return {
       x: x + radius * Math.sin(angle * Math.PI / 180) - radius * Math.sin(baseAngle * Math.PI / 180),
       y: y - radius * Math.cos(angle * Math.PI / 180) + radius * Math.cos(baseAngle * Math.PI / 180),
@@ -16,45 +20,38 @@ export abstract class HandLocator<P extends number = number, M extends number = 
     }
   }
 
-  getItemRotateZ(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): number {
-    return this.getItemAngle(item, context)
-  }
-
-  getItemAngle(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): number {
-    const index = this.getItemIndex(item, context)
-    const size = this.countItems(item.location, context)
-    const maxAngle = this.getMaxAngle(item, context)
-    const gapMaxAngle = this.getGapMaxAngle(item, context)
+  getRotateZ(location: Location<P, L>, context: MaterialContext<P, M, L>): number {
+    const index = this.getLocationIndex(location, context)
+    const baseAngle = this.getBaseAngle(location, context)
+    if (index === undefined) return baseAngle
+    const size = this.countItems(location, context)
+    const maxAngle = this.getMaxAngle(location, context)
+    const gapMaxAngle = this.getGapMaxAngle(location, context)
     const gapAngle = size > 1 ? Math.min(maxAngle / (size - 1), gapMaxAngle) : 0
-    const baseAngle = this.getBaseAngle(item, context)
-    return baseAngle + (index - (size - 1) / 2) * gapAngle * (this.isClockwise(item, context) ? 1 : -1)
+    return baseAngle + (index - (size - 1) / 2) * gapAngle * (this.isClockwise(location, context) ? 1 : -1)
   }
 
-  getItemIndex(item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number {
-    return item.location.x ?? 0
-  }
-
-  getRadius(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number {
+  getRadius(_location: Location<P, L>, _context: MaterialContext<P, M, L>): number {
     return 100
   }
 
-  getBaseAngle(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number {
+  getBaseAngle(_location: Location<P, L>, _context: MaterialContext<P, M, L>): number {
     return 0
   }
 
-  getMaxAngle(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number {
+  getMaxAngle(_location: Location<P, L>, _context: MaterialContext<P, M, L>): number {
     return 15
   }
 
-  getGapMaxAngle(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number {
+  getGapMaxAngle(_location: Location<P, L>, _context: MaterialContext<P, M, L>): number {
     return 3
   }
 
-  isClockwise(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): boolean {
+  isClockwise(_location: Location<P, L>, _context: MaterialContext<P, M, L>): boolean {
     return true
   }
 
-  getDeltaZ(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number {
+  getDeltaZ(_location: Location<P, L>, _context: MaterialContext<P, M, L>): number {
     return 0.05
   }
 }
