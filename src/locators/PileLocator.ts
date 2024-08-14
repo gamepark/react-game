513164c33c935ca1cb4950791b088d722ce9da93
Romain Fromi <romain.fromi@gmail.com>
2024-08-14 22:42:18 +0200

    Refactor PileLocator the same way as HandLocator and LineLocator

--- src/locators/PileLocator.ts
+++ src/locators/PileLocator.ts
@@ -1,19 +1,40 @@
-import { Coordinates, MaterialItem, XYCoordinates } from '@gamepark/rules-api'
-import { ItemContext, Locator } from './Locator'
+import { Coordinates, Location, MaterialItem, XYCoordinates } from '@gamepark/rules-api'
+import { ItemContext, Locator, MaterialContext } from './Locator'
 
 export abstract class PileLocator<P extends number = number, M extends number = number, L extends number = number> extends Locator<P, M, L> {
   limit = 20
   private positions = new Map<string, Map<number, Coordinates>>()
   private rotations = new Map<string, Map<number, number>>()
 
-  getItemCoordinates(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): Coordinates {
-    const pileId = this.getPileId(item, context)
+  radius: number | XYCoordinates = 0
+
+  getRadius(_location: Location<P, L>, _context: MaterialContext<P, M, L>): number | XYCoordinates {
+    return this.radius
+  }
+
+  maxAngle = 180
+
+  getMaxAngle(_location: Location<P, L>, _context: MaterialContext<P, M, L>): number {
+    return this.maxAngle
+  }
+
+  getPileId(location: Location<P, L>, _context: MaterialContext<P, M, L>): string {
+    return [location.player, location.id, location.parent].filter(part => part !== undefined).join('_')
+  }
+
+  getPileCoordinates(_location: Location<P, L>, _context: MaterialContext<P, M, L>) {
+    return this.coordinates
+  }
+
+  getLocationCoordinates(location: Location<P, L>, context: MaterialContext<P, M, L>,
+                         index = this.getLocationIndex(location, context)): Partial<Coordinates> {
+    if (index === undefined) return this.getPileCoordinates(location, context)
+    const pileId = this.getPileId(location, context)
     if (!this.positions.has(pileId)) this.positions.set(pileId, new Map())
     const pilePositions = this.positions.get(pileId)!
-    const index = this.getItemIndex(item, context)
-    const radius = this.getRadius(item, context)
+    const radius = this.getRadius(location, context)
     if (!pilePositions.has(index)) {
-      const { x = 0, y = 0, z = 0 } = this.getLocationCoordinates(item.location, context)
+      const { x = 0, y = 0, z = 0 } = this.getPileCoordinates(location, context)
       const distance = Math.random()
       const direction = Math.random() * 2 * Math.PI
       pilePositions.set(index, {
@@ -25,32 +46,24 @@ export abstract class PileLocator<P extends number = number, M extends number = 
     return pilePositions.get(index)!
   }
 
-  getItemRotateZ(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): number {
+  getItemCoordinates(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): Partial<Coordinates> {
+    return this.getLocationCoordinates(item.location, context, this.getItemIndex(item, context))
+  }
+
+  getRotateZ(location: Location<P, L>, context: MaterialContext<P, M, L>,
+             index = this.getLocationIndex(location, context) ?? 0): number {
     if (!this.maxAngle) return 0
-    const pileId = this.getPileId(item, context)
+    const pileId = this.getPileId(location, context)
     if (!this.rotations.has(pileId)) this.rotations.set(pileId, new Map())
     const pileRotations = this.rotations.get(pileId)!
-    const index = this.getItemIndex(item, context)
     if (!pileRotations.has(index)) {
-      const maxAngle = this.getMaxAngle(item, context)
+      const maxAngle = this.getMaxAngle(location, context)
       pileRotations.set(index, (Math.random() - 0.5) * maxAngle)
     }
     return pileRotations.get(index) ?? 0
   }
 
-  radius: number | XYCoordinates = 0
-
-  getRadius(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number | XYCoordinates {
-    return this.radius
-  }
-
-  maxAngle = 180
-
-  getMaxAngle(_item: MaterialItem<P, L>, _context: ItemContext<P, M, L>): number {
-    return this.maxAngle
-  }
-
-  getPileId(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): string {
-    return [item.location.player, item.location.id, item.location.parent, context.type].filter(part => part !== undefined).join('_')
+  getItemRotateZ(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): number {
+    return this.getRotateZ(item.location, context, this.getItemIndex(item, context))
   }
 }
