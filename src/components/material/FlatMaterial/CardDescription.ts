5631988bfde149421a4f7051658a048566dfd5d2
Romain Fromi <romain.fromi@gmail.com>
2024-08-17 11:42:59 +0200

    Define `CardDescription.height` instead of ratio to use it easily in game

--- src/components/material/FlatMaterial/CardDescription.ts
+++ src/components/material/FlatMaterial/CardDescription.ts
@@ -3,6 +3,6 @@ import { FlatMaterialDescription } from './FlatMaterial'
 export abstract class CardDescription<P extends number = number, M extends number = number, L extends number = number, ItemId = any>
   extends FlatMaterialDescription<P, M, L, ItemId> {
   width = 6.35
-  ratio = 5 / 7
+  height = 8.89
   borderRadius = 0.4
 }
