60dd88132a9489cc245d1fea738c049f9ff87ea6
Romain Fromi <romain.fromi@gmail.com>
2024-09-12 11:30:09 +0200

    Fix default cards width

--- src/components/material/FlatMaterial/CardDescription.ts
+++ src/components/material/FlatMaterial/CardDescription.ts
@@ -2,7 +2,7 @@ import { FlatMaterialDescription } from './FlatMaterial'
 
 export class CardDescription<P extends number = number, M extends number = number, L extends number = number, ItemId = any>
   extends FlatMaterialDescription<P, M, L, ItemId> {
-  width = 6.35
+  width = 6.3
   height = 8.8
   borderRadius = 0.4
 }
