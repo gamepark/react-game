8fa95da1db72a74c1503f2aa6a888ef3ab102353
Patrick Beaumé <p.beaume@gmail.com>
2025-05-19 12:38:21 +0200

    add sound based on material (#125)
    * add sound based on material
    
    * remove dead code

--- src/components/menus/SoundButton/SoundButton.tsx
+++ src/components/menus/SoundButton/SoundButton.tsx
@@ -3,16 +3,12 @@ import { faVolumeMute } from '@fortawesome/free-solid-svg-icons/faVolumeMute'
 import { faVolumeUp } from '@fortawesome/free-solid-svg-icons/faVolumeUp'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useSoundControls } from '@gamepark/react-client'
-import { useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 import { menuButtonCss } from '../menuCss'
-import { gameContext } from '../../GameProvider'
 
 export const SoundButton = () => {
   const { t } = useTranslation()
   const { mute, unmute, muted } = useSoundControls()
-  const hasSounds = useContext(gameContext)?.hasSounds
-  if (!hasSounds) return null
   return (
     <button css={menuButtonCss} onClick={() => muted ? unmute() : mute()}>
       <FontAwesomeIcon icon={muted ? faVolumeMute : faVolumeUp}/>
