a8cd05963c094daeb82c404303203d16cccfe022
Romain Fromi <romain.fromi@gmail.com>
2025-05-30 11:49:45 +0200

    Bugfix HexagonalGridLocator

--- src/locators/HexagonalGridLocator.ts
+++ src/locators/HexagonalGridLocator.ts
@@ -1,19 +1,9 @@
-import {
-  Coordinates,
-  GridBoundaries,
-  hexFromAxial,
-  HexGridSystem,
-  hexRotate,
-  hexToAxial,
-  Location,
-  MaterialItem,
-  MoveItem,
-  XYCoordinates
-} from '@gamepark/rules-api'
+import { Coordinates, GridBoundaries, HexGridSystem, Location, MaterialItem, MoveItem, XYCoordinates } from '@gamepark/rules-api'
 import isEqual from 'lodash/isEqual'
 import maxBy from 'lodash/maxBy'
 import minBy from 'lodash/minBy'
 import omit from 'lodash/omit'
+import range from 'lodash/range'
 import uniqWith from 'lodash/uniqWith'
 import { HexGridDropAreaDescription, isPolyhexDescription, LocationDescription } from '../components'
 import { ItemContext, Locator, MaterialContext } from './Locator'
@@ -64,34 +54,18 @@ export abstract class HexagonalGridLocator<P extends number = number, M extends 
   }
 
   /**
-   * Get the delta X and Y coordinates to apply to place some area given its boundaries.
-   * @param boundaries Boundaries of the location to place
-   * @return X and Y delta to apply
+   * Based on the coordinates of each hexagon in an area, return the delta coordinates to place the area properly
+   * @param areaCoordinates Coordinates of each hexagon composing the area
+   * @param coordinatesSystem System of coordinates to consider
+   * @return delta x and y to apply to the position of the area
    */
-  private getAreaDelta(boundaries: Partial<GridBoundaries>): XYCoordinates {
-    const { xMin = 0, xMax = 0, yMin = 0, yMax = 0 } = boundaries
-    switch (this.coordinatesSystem) {
-      case HexGridSystem.Axial: {
-        return { x: 0, y: 0 }
-        // throw new Error('Axial HexGridSystem is not yet implemented')
-      }
-      case HexGridSystem.OddQ: {
-        const deltaX = (xMin + xMax) / 2
-        const deltaY = ((xMin !== xMax ? 0.5 : xMin % 2 === 0 ? 0 : 1) + yMin + yMax) / 2
-        return { x: deltaX * 3 / 2 * this.sizeX, y: deltaY * Math.sqrt(3) * this.sizeY }
-      }
-      case HexGridSystem.EvenQ: {
-        const deltaX = (xMin + xMax) / 2
-        const deltaY = ((xMin !== xMax ? -0.5 : xMin % 2 === 0 ? 0 : -1) + yMin + yMax) / 2
-        return { x: deltaX * 3 / 2 * this.sizeX, y: deltaY * Math.sqrt(3) * this.sizeY }
-      }
-      case HexGridSystem.OddR: {
-        throw new Error('OddR HexGridSystem is not yet implemented')
-      }
-      case HexGridSystem.EvenR: {
-        throw new Error('EvenR HexGridSystem is not yet implemented')
-      }
-    }
+  getAreaDelta(areaCoordinates: XYCoordinates[], coordinatesSystem = this.coordinatesSystem): XYCoordinates {
+    const areaDeltaCoordinates = areaCoordinates.map((coordinates) => this.getHexagonPosition(coordinates, coordinatesSystem))
+    const xMin = minBy(areaDeltaCoordinates, 'x')?.x ?? 0
+    const xMax = maxBy(areaDeltaCoordinates, 'x')?.x ?? 0
+    const yMin = minBy(areaDeltaCoordinates, 'y')?.y ?? 0
+    const yMax = maxBy(areaDeltaCoordinates, 'y')?.y ?? 0
+    return { x: (xMin + xMax) / 2, y: (yMin + yMax) / 2 }
   }
 
   /**
@@ -102,7 +76,9 @@ export abstract class HexagonalGridLocator<P extends number = number, M extends 
    */
   getAreaCoordinates(location: Location<P, L>, context: MaterialContext<P, M, L>): Partial<Coordinates> {
     const { x = 0, y = 0, z } = this.getCoordinates(location, context)
-    const { x: deltaX = 0, y: deltaY = 0 } = this.getAreaDelta(this.getBoundaries(location, context))
+    const { xMax = 0, xMin = 0, yMax = 0, yMin = 0 } = this.getBoundaries(location, context)
+    const areaCoordinates = range(xMin, xMax + 1).flatMap((x) => range(yMin, yMax + 1).map((y) => ({ x, y })))
+    const { x: deltaX, y: deltaY } = this.getAreaDelta(areaCoordinates)
     return { x: x + deltaX, y: y + deltaY, z }
   }
 
@@ -117,29 +93,37 @@ export abstract class HexagonalGridLocator<P extends number = number, M extends 
       return this.getAreaCoordinates(location, context)
     }
     const { x: baseX = 0, y: baseY = 0, z: baseZ } = this.getCoordinates(location, context)
-    let { x = 0, y = 0 } = location
-    switch (this.coordinatesSystem) {
+    const { x = 0, y = 0 } = this.getHexagonPosition(location)
+    return { x: baseX + x, y: baseY + y, z: baseZ }
+  }
+
+  /**
+   * Get the position of a hexagon
+   * @param coordinates Coordinates of the hexagon
+   * @param coordinatesSystem System of coordinates to consider
+   * @return coordinates to place the hexagon on the location
+   */
+  getHexagonPosition(coordinates: Partial<XYCoordinates>, coordinatesSystem = this.coordinatesSystem): XYCoordinates {
+    let { x = 0, y = 0 } = coordinates
+    switch (coordinatesSystem) {
       case HexGridSystem.Axial: {
         return {
-          x: baseX + (x * Math.sqrt(3) + y * Math.sqrt(3) / 2) * this.sizeX,
-          y: baseY + y * 3 / 2 * this.sizeY,
-          z: baseZ
+          x: (x * Math.sqrt(3) + y * Math.sqrt(3) / 2) * this.sizeX,
+          y: y * 3 / 2 * this.sizeY
         }
       }
       case HexGridSystem.OddQ: {
         y += ((x % 2 + 2) % 2) / 2
         return {
-          x: baseX + x * 3 / 2 * this.sizeX,
-          y: baseY + y * Math.sqrt(3) * this.sizeY,
-          z: baseZ
+          x: x * 3 / 2 * this.sizeX,
+          y: y * Math.sqrt(3) * this.sizeY
         }
       }
       case HexGridSystem.EvenQ: {
         y -= ((x % 2 + 2) % 2) / 2
         return {
-          x: baseX + x * 3 / 2 * this.sizeX,
-          y: baseY + y * Math.sqrt(3) * this.sizeY,
-          z: baseZ
+          x: x * 3 / 2 * this.sizeX,
+          y: y * Math.sqrt(3) * this.sizeY
         }
       }
       case HexGridSystem.OddR: {
@@ -152,29 +136,22 @@ export abstract class HexagonalGridLocator<P extends number = number, M extends 
   }
 
   /**
-   * Get the coordinates of an item on the grid. If the item is a Polyhex, the shape will be use to center the origin coordinates of the item on the
+   * Get the coordinates of an item on the grid. If the item is a Polyhex, the shape will be used to center the origin coordinates of the item on the
    * correct grid location.
    * @param item Item to place
    * @param context Context of the game
    * @return the coordinates of the item
    */
   getItemCoordinates(item: MaterialItem<P, L>, context: ItemContext<P, M, L>): Partial<Coordinates> {
+    const { x = 0, y = 0, z } = super.getItemCoordinates(item, context)
     const description = context.material[context.type]
-    if (description && isPolyhexDescription<P, M, L>(description)) {
-      const shape = description.getPolyhexShape(item, context).map(hex =>
-        hexFromAxial(
-          hexRotate(
-            hexToAxial(hex, description.coordinatesSystem),
-            item.location.rotation
-          ),
-          this.coordinatesSystem)
-      )
-      const xMin = minBy(shape, 'x')?.x, xMax = maxBy(shape, 'x')?.x, yMin = minBy(shape, 'y')?.y, yMax = maxBy(shape, 'y')?.y
-      const { x = 0, y = 0, z } = super.getItemCoordinates(item, context)
-      const { x: deltaX = 0, y: deltaY = 0 } = this.getAreaDelta({ xMin, xMax, yMin, yMax })
-      return { x: x + deltaX, y: y + deltaY, z }
+    if (!description || !isPolyhexDescription<P, M, L>(description)) {
+      return { x, y, z }
     }
-    return super.getItemCoordinates(item, context)
+    const shape = description.getPolyhexShape(item, context)
+    const areaDelta = this.getAreaDelta(shape, description.coordinatesSystem)
+    const { x: deltaX, y: deltaY } = rotateVector(areaDelta, item.location.rotation * 60)
+    return { x: x + deltaX, y: y + deltaY, z }
   }
 
   /**
@@ -226,5 +203,15 @@ export abstract class HexagonalGridLocator<P extends number = number, M extends 
         throw new Error('EvenR HexGridSystem is not yet implemented')
       }
     }
+  }
+}
+
+function rotateVector(vector: XYCoordinates, degrees: number) {
+  const rad = degrees * (Math.PI / 180)
+  const cos = Math.cos(rad)
+  const sin = Math.sin(rad)
+  return {
+    x: Math.round(10000 * (vector.x * cos - vector.y * sin)) / 10000,
+    y: Math.round(10000 * (vector.x * sin + vector.y * cos)) / 10000
   }
 }
