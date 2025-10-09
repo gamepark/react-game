44926facd13fde708473fa8589ac84d676609afd
Romain Fromi <romain.fromi@gmail.com>
2025-10-09 13:14:53 +0200

    Fix animation to different origins for create and delete items

--- src/components/material/animations/getFirstStockItemTransforms.util.ts
+++ src/components/material/animations/getFirstStockItemTransforms.util.ts
@@ -1,3 +1,4 @@
+import { MaterialItem } from '@gamepark/rules-api'
 import { isEqual } from 'es-toolkit'
 import { getItemFromContext, ItemContext } from '../../../locators'
 
@@ -11,4 +12,14 @@ export const getFirstStockItemTransforms = <P extends number = number, M extends
   const stockItem = description?.getStaticItems(context).find(item => isEqual(item.location, stockLocation))
   const displayIndex = stockItem?.quantity ? stockItem.quantity - 1 : 0
   return description.getItemTransform(stockItem ?? { location: stockLocation }, { ...context, index: 0, displayIndex })
+}
+
+export const getFirstStockItem = <P extends number = number, M extends number = number, L extends number = number>(
+  context: ItemContext<P, M, L>
+): MaterialItem<P, L> | undefined => {
+  const description = context.material[context.type]
+  const item = getItemFromContext(context)
+  const stockLocation = description?.getStockLocation(item, context)
+  if (!description || !stockLocation) return
+  return description?.getStaticItems(context).find(item => isEqual(item.location, stockLocation))
 }
